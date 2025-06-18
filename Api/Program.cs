c9d331dae89c4d4a4f0947623082efb0e1f6b100
ibadell <ibadell@virtuoso.com>
2025-06-18 20:18:24 -0300

    Project Init

---
+++ Api/Program.cs
@@ -0,0 +1,14 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
