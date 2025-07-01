37fe9dd0512867d0938fca9347ac1bde86aff9ef
ibadell <ibadell@virtuoso.com>
2025-06-30 23:37:33 -0300

    Adding JWT validation and swagger ui

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,13 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
+builder.Services
+	.AddAuthorization()
+	.AddAuthentication(options =>
+	{
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options =>
+	{
+		options.Authority = config["Auth0Config:Authority"];
+		options.Audience = config["Auth0Config:Audience"];
+	});
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+	// Add JWT Bearer support in Swagger UI
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Please enter JWT with Bearer into field. Example: 'Bearer {token}'",
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+
+	});
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] {}
+		}
+	});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+app.MapGet("/secure", () => "Authenticated!").RequireAuthorization();
 
 app.UseHttpsRedirection();
 
